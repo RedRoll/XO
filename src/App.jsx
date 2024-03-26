881fae5bb08ca8c6a5dc2c294fee74338d074eb0
RedRoll <redrollofficial@gmail.com>
2024-03-26 13:00:17 +0100

    Add: base functionality for gameBoard cells

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import styles from './App.module.css'
 import { useState } from 'react'
 
 import Player from './components/Player/Player'
+import GameTable from './components/Gameboard/GameTable'
 
 import logo from './assets/LOGO.png'
+import { WINNING_COMBINATIONS } from './WINNING_COMBINATIONS'
 
 const INPUT_DATA = {
   player1: {
@@ -18,17 +20,56 @@ const INPUT_DATA = {
   toggle: ''
 }
 
+const DEFAULT_GAMETABLE = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
+// auxiliary function that adds changes to the game board (when 1 of the players click on some square on gameBoard)
+const setGameTable = data => {
+  const gameTable = [...DEFAULT_GAMETABLE.map(item => [...item])] // deep cloning
+
+  for (const item of data) {
+    const {curSymbol, square} = item
+    const {row, col} = square
+    gameTable[row][col] = curSymbol
+  }
+
+  return gameTable
+}
+
 
 function App() {
 
   let playerData = structuredClone(INPUT_DATA)
 
-  const [player, setPlayer] = useState(playerData)
+  const [player, setPlayer] = useState(playerData) // player data (current symbol, name....))
 
-  const clickHandlerReset = () =>  setPlayer(() => playerData) 
+  const [activePlayer, setActivePlayer] = useState('player1') // current active Player (in this moment)
 
-  console.log(player.toggle)
-  
+  const [gameSquareData, setGameSquareData] = useState([]) // logs of all clicks with playerName, symbol etc
+
+// handling player clicks on gameTable cells
+  const getDataOnClick = (rowIndex, colIndex) => {
+   
+    setActivePlayer(() => activePlayer === 'player1' ? 'player2' : 'player1')
+
+    setGameSquareData(prevState => {
+      // let namePlayer = player[activePlayer].playerName
+      let symbolPlayer = player[activePlayer].playerSymbol
+      let playerTurnsData = [...prevState, { curSymbol: symbolPlayer, square: { row: rowIndex, col: colIndex } }]
+
+      return playerTurnsData
+    })
+  }
+
+  console.log(gameSquareData)
+
+  const gameTable = setGameTable(gameSquareData) // re-run every time the state changes
+
+  const clickHandlerReset = () => setPlayer(() => playerData) // reset data on button 'reset' click
+
   return (
     <main className={styles.wrapper}>
 
@@ -38,12 +79,14 @@ function App() {
       <div className={styles['game-wrapper']}>
 
         <div className={styles.game__players}>
-          {/* додати логіку, якщо в одному playerline відкриті nput, то в іншому playerline заборонити відкривати доки не закриються в першому */}
+          
           <Player toggleValue={player.toggle} id='player1' name='Player 1' symbol='X' player={player.player1} setPlayer={setPlayer} />
           <Player toggleValue={player.toggle} id='player2' name='Player 2' symbol='O' player={player.player2} setPlayer={setPlayer} />
-        
+
         </div>
         {/* <PlayerLine /> */}
+
+        <GameTable gameTable={gameTable} onSquareClick={getDataOnClick} />
 
 
         <button onClick={clickHandlerReset} >Reset</button>
---
+++ src/components/Gameboard/GameTable.jsx
@@ -0,0 +1,27 @@
+import styles from './GameTable.module.css'
+
+
+const GameBoard = ({ gameTable, onSquareClick }) => {
+
+    return (
+        <ol className={styles.board}>
+
+            {gameTable.map((row, rowIndex) => <li key={rowIndex}> {/* 3 rows*/} 
+
+                <ol className={styles.board__row}>
+                    
+                    {row.map((symbol, colIndex) => <li className={styles.row__symbol} key={colIndex}> {/*3 items in row*/} 
+
+                        <button onClick={ () => onSquareClick(rowIndex, colIndex)} className={styles.symbol__button}>{symbol}</button> {/*cell in item*/} 
+
+                    </li>)}
+
+                </ol>
+
+            </li>)}
+
+        </ol>
+    )
+}
+
+export default GameBoard
