cecf53eb95f104a3200c55631ecd48f26586a68e
RedRoll <redrollofficial@gmail.com>
2024-03-29 12:27:54 +0100

    Test2: RWD

--- src/components/Gameboard/GameTable.jsx
+++ src/components/Gameboard/GameTable.jsx
@@ -6,13 +6,17 @@ const GameBoard = ({ gameTable, onSquareClick }) => {
     return (
         <ol className={styles.board}>
 
-            {gameTable.map((row, rowIndex) => <li className={styles.board__wrapper} key={rowIndex}> {/* 3 rows*/} 
+            {gameTable.map((row, rowIndex) => <li className={styles.board__wrapper} key={rowIndex}> {/* 3 rows*/}
 
                 <ol className={styles.board__row}>
-                    
-                    {row.map((symbol, colIndex) => <li className={styles.row__symbol} key={colIndex}> {/*3 items in row*/} 
 
-                        <button onClick={ () => onSquareClick(rowIndex, colIndex)} className={styles.symbol__button} disabled={symbol !== null}>{symbol}</button> {/*cell in item*/} 
+                    {row.map((symbol, colIndex) => <li className={styles.row__symbol} key={colIndex}> {/*3 items in row*/}
+
+                        <button onClick={() => onSquareClick(rowIndex, colIndex)} className={styles.symbol__button} disabled={symbol !== null}>
+                            <span className={styles.button__item}>
+                                <span className={styles.item__inner}>{symbol}</span>
+                            </span>
+                        </button> {/*cell in item*/}
 
                     </li>)}
 
