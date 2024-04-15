cf2b2c71dbf28660fc6e9fc6ba0a631b1c3a9a1b
RedRoll <redrollofficial@gmail.com>
2024-04-15 12:57:15 +0200

    Add: RWD styling

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -61,7 +61,7 @@ const Player = ({ name, symbol, id, player, toggleValue, setPlayer, activeP }) =
 
     }
 
-    const handlerClosePopUp = () => setToggle( prevState => { return {...prevState, popUp: !prevState.popUp} } )
+    const handlerClosePopUp = () => setToggle(prevState => { return { ...prevState, popUp: !prevState.popUp } })
 
     const clickHandlerInput = item => {
         setToggle(prevState => { return { ...prevState, inputClicked: !prevState.inputClicked } }),
@@ -81,8 +81,9 @@ const Player = ({ name, symbol, id, player, toggleValue, setPlayer, activeP }) =
 
     const isTrue = toggleValue !== id && toggleValue.length > 0 // for switch button disable/enable
 
-    const disabledClass = isTrue ? styles['player__nameText-disabled'] : undefined
+    const disabledClass = isTrue ? styles['input__nameText-disabled'] : undefined
 
+    
     return (
 
         <div className={`${styles.player} ${activeP && activeP === id ? styles['active-player'] : undefined}`}>
@@ -90,25 +91,35 @@ const Player = ({ name, symbol, id, player, toggleValue, setPlayer, activeP }) =
             <button disabled={isTrue} className={styles.player__button} onClick={clickHandlerButton} >{toggle.input ? 'Save' : 'Edit'}</button>
 
 
-            {
+            <div className={styles['players-box']}>
 
-                toggle.input ? // name
+                <span className={styles.player__input}>
+                    <p>Name:</p>
+                    {
 
-                    <input className={`${styles.player__name} ${animationClass}`} value={player.playerName} type="text" onChange={event => changeHandler(event, Object.keys(player)[0])} onClick={() => clickHandlerInput(Object.keys(player)[0])} placeholder='Name' required />
+                        toggle.input ? // name
 
-                    :
+                            <input className={`${styles.input__name} ${animationClass}`} value={player.playerName} type="text" onChange={event => changeHandler(event, Object.keys(player)[0])} onClick={() => clickHandlerInput(Object.keys(player)[0])} placeholder='Name' required maxLength={10} />
 
-                    <span className={`${styles.player__nameText} ${disabledClass}`}>{player.playerName.length <= 0 ? name : player.playerName}</span>
+                            :
 
-            }
+                            <span className={`${styles.input__nameText} ${disabledClass}`}>{player.playerName.length <= 0 ? name : player.playerName}</span>
 
-            {toggle.input ? // symbol
+                    }
+                </span>
 
-                <input className={`${styles.player__symbol} ${animationClass}`} value={player.playerSymbol} type="text" onChange={event => changeHandler(event, Object.keys(player)[1])} onClick={() => clickHandlerInput(Object.keys(player)[1])} placeholder='Symbol' required />
+                <span className={styles.player__input}>
+                    <p>Symbol:</p>
+                    {toggle.input ? // symbol
 
-                :
+                        <input className={`${styles.input__symbol} ${animationClass}`} value={player.playerSymbol} type="text" onChange={event => changeHandler(event, Object.keys(player)[1])} onClick={() => clickHandlerInput(Object.keys(player)[1])} placeholder='Symbol' required />
 
-                <span className={`${styles.player__nameText} ${disabledClass}`}>{player.playerSymbol.length <= 0 ? symbol : player.playerSymbol}</span>}
+                        :
+
+                        <span className={`${styles.input__nameText} ${disabledClass}`}>{player.playerSymbol.length <= 0 ? symbol : player.playerSymbol}</span>}
+                </span>
+
+            </div>
 
 
 
